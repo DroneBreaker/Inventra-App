056fa4b3039cdd522c16f83997137f1d8ebc0cc9
DroneBreaker <brownierz01@gmail.com>
2025-05-15 19:53:06 +0100

    Implement invoice creation page

--- backend/src/models/invoice.rs
+++ backend/src/models/invoice.rs
@@ -4,6 +4,39 @@ use rust_decimal::Decimal;
 
 use super::item::Item;
 
+#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+pub struct Invoice {
+    pub id: String,
+    pub invoice_number: String,
+    pub seller: String,
+    pub company_tin: String,
+    pub client_name: String,
+    pub client_tin: String,
+    pub invoice_date: DateTime<Utc>,
+    pub due_date: DateTime<Utc>,
+    #[sqlx(default)]
+    pub total_vat: Decimal, 
+    #[sqlx(default)]
+    pub total_amount: Decimal,
+    pub status: InvoiceStatus,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+pub struct InvoiceItem {
+    pub id: String,
+    pub invoice_id: String, // foreign key to Invoice
+    pub item_id: String,    // foreign key to Item
+    pub quantity: i32,
+    pub unit_price: Decimal,
+    pub tax_amount: Decimal,
+    pub discount: Decimal,
+    pub line_total: Decimal, // (quantity * unit_price) - discount + tax
+}
+
+
+
 #[derive(Debug, Serialize, Deserialize)]
 pub enum InvoiceStatus {
     Draft,
@@ -14,22 +47,4 @@ pub enum InvoiceStatus {
 }
 
 
-#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
-pub struct Invoice {
-    pub id: String,
-    pub invoice_number: String,
-    pub client_name: String,
-    pub client_tin: String,
-    pub invoice_date: DateTime<Utc>,
-    pub due_date: DateTime<Utc>,
-    pub quantity: i32,
-    pub price: Decimal,
-    pub vat: Decimal, 
-    #[sqlx(default)]
-    pub sub_total: Decimal,
-    #[sqlx(default)]
-    pub total_amount: Decimal,
-    pub status: InvoiceStatus,
-    pub items: Vec<Item>
-}
 
--- mobile/lib/screens/create_invoice.dart
+++ mobile/lib/screens/create_invoice.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:inventra/constants/app_titles.dart';
+import 'package:inventra/widgets/app_text.dart';
 
 class CreateInvoice extends StatefulWidget {
   const CreateInvoice({super.key});
@@ -8,13 +11,15 @@ class CreateInvoice extends StatefulWidget {
 }
 
 class _CreateInvoiceState extends State<CreateInvoice> {
-  final TextEditingController dateController = TextEditingController();
-  final TextEditingController timeController = TextEditingController();
-  final TextEditingController businessPartnerController =
-      TextEditingController();
-  final TextEditingController businessTINController = TextEditingController();
-  final TextEditingController usernameController = TextEditingController();
   final TextEditingController invoiceNumberController = TextEditingController();
+  final TextEditingController sellerController = TextEditingController();
+  final TextEditingController clientNameController =
+      TextEditingController();
+  final TextEditingController clientTINController = TextEditingController();
+  final TextEditingController invoiceDateController = TextEditingController();
+  final TextEditingController invoiceTimeController = TextEditingController();
+  final TextEditingController dueDateController= TextEditingController();
+  final TextEditingController totalVATController = TextEditingController();
   final TextEditingController totalAmountController = TextEditingController();
   final TextEditingController itemCodeController = TextEditingController();
   final TextEditingController itemNameController = TextEditingController();
@@ -29,8 +34,62 @@ class _CreateInvoiceState extends State<CreateInvoice> {
   final _formKey = GlobalKey<FormState>();
 
   // DATE OPTIONS
-  DateTime? selectedDate;
-  TimeOfDay? selectedTime;
+  DateTime? selectedInvoiceDate;
+  DateTime? selectedDueDate;
+  TimeOfDay? selectedInvoiceTime;
+
+  // invoice Date picker
+  Future<void> _selectInvoiceDate(BuildContext context) async {
+    final DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (date != null) {
+      setState(() {
+        // Format the picked date and display it in the text field
+        invoiceDateController.text = DateFormat('yyyy-MM-dd').format(date);
+        selectedInvoiceDate = date;
+      });
+    }
+  }
+
+
+  // Due Date picker
+  Future<void> _selectDueDate(BuildContext context) async {
+    final DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (date != null) {
+      setState(() {
+        // Format the picked date and display it in the text field
+        dueDateController.text = DateFormat('yyyy-MM-dd').format(date);
+        selectedDueDate = date;
+      });
+    }
+  }
+
+  // Time picker
+  Future<void> _selectInvoiceTime(BuildContext context) async {
+    final TimeOfDay? time =
+        await showTimePicker(context: context, initialTime: TimeOfDay.now());
+
+    if (time != null) {
+      // Format the TimeOfDay manually to 'HH:mm'
+      final String formattedTime = time.format(context);
+
+      setState(() {
+        invoiceDateController.text = formattedTime;
+        selectedInvoiceTime = time;
+      });
+    }
+  }
 
   // OPTIONS
   bool isTaxInclusive = true;
@@ -51,15 +110,15 @@ class _CreateInvoiceState extends State<CreateInvoice> {
   final List<String> currencyOptions = ["GHS", "USD", "EUR", "GBP"];
   String selectedCurrency = "GHS";
 
-  // BUSINESS PARTNER OPTIONS
-  String selectedBusinessPartner = "Customer";
-  final List<String> businessPartnerOptions = [
+  // CLIENT OPTIONS
+  String selectedClient = "Customer";
+  final List<String> clientOptions = [
     'Customer',
     'Supplier',
     "Exempt"
   ];
-  List<Map<String, dynamic>> partners = [];
-  List<Map<String, dynamic>> filteredPartners = [];
+  List<Map<String, dynamic>> clients = [];
+  List<Map<String, dynamic>> filteredClients = [];
 
   // FLAG OPTIONS
   String? selectedFlag;
@@ -82,22 +141,262 @@ class _CreateInvoiceState extends State<CreateInvoice> {
     },
   ];
 
+  // INVOICE STATUS OPTIONS
+  String selectedInvoiceStatus = "Draft";
+  final List<String> invoiceStatus = [
+    "Draft",
+    "Sent",
+    "Paid",
+    "Overdue",
+    "Canceled",
+  ];
+
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top,
-            child: Column(
-              children: [
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+                children: [
+                  Positioned(
+                    top: 10,
+                    left: 8,
+                    child: Icon(Icons.arrow_back, size: 40,),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20, top: 70),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          AppText(title: "INVOICE FUCKERS"),
+                          SizedBox(height: 10,),
+                      
 
-              ],
-            ),
-          ),
+                          // Invoice Number TextForm field
+                          TextFormField(
+                            controller: invoiceNumberController,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 20),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Invoice Number"
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Seller TextForm field
+                          TextFormField(
+                            controller: sellerController,
+                            enabled: false,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 20),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Seller"
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Client TextForm field
+                          TextFormField(
+                            controller: clientNameController,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 20),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Client Name"
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Client TIN
+                          TextFormField(
+                            controller: clientTINController,
+                            enabled: false,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 20),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Client TIN"
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Invoice Date TextForm field
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Form(
+                                  child: TextFormField(
+                                      controller: invoiceDateController,
+                                      keyboardType: TextInputType.datetime,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10)
+                                        ),
+                                        contentPadding: EdgeInsets.only(top: 40, left: 20),
+                                        hintText: "Invoice Date"
+                                      ),
+                                      onChanged: (value) {
+                                        print('Invoice Date: $value');
+                                      },
+                                    ),
+                                ),
+                              ),
+                              IconButton(onPressed: () => _selectInvoiceDate(context), 
+                                icon: Icon(Icons.calendar_month, size: 30,)
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Invoice Time TextForm field
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Form(
+                                  child: TextFormField(
+                                      controller: invoiceTimeController,
+                                      keyboardType: TextInputType.datetime,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10)
+                                        ),
+                                        contentPadding: EdgeInsets.only(top: 40, left: 20),
+                                        hintText: "Invoice Time"
+                                      ),
+                                      onChanged: (value) {
+                                        print('Invoice Time: $value');
+                                      },
+                                    ),
+                                ),
+                              ),
+                              IconButton(onPressed: () => _selectInvoiceTime(context), 
+                                icon: Icon(Icons.calendar_month, size: 30,)
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Due Date TextForm field
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Form(
+                                  child: TextFormField(
+                                      controller: dueDateController,
+                                      keyboardType: TextInputType.datetime,
+                                      decoration: InputDecoration(
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(10)
+                                        ),
+                                        contentPadding: EdgeInsets.only(top: 40, left: 20),
+                                        hintText: "Due Date"
+                                      ),
+                                      onChanged: (value) {
+                                        print('Due Date: $value');
+                                      },
+                                    ),
+                                ),
+                              ),
+                              IconButton(onPressed: () => _selectDueDate(context), 
+                                icon: Icon(Icons.calendar_month, size: 30,)
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Total VAT TextForm field
+                          TextFormField(
+                            controller: totalVATController,
+                            enabled: false,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 20),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Total VAT"
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Total Amount TextForm field
+                          TextFormField(
+                            controller: totalAmountController,
+                            enabled: false,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 20),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Total Amount"
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Invoice Status Dropdown
+                          DropdownButtonFormField(
+                            value: selectedInvoiceStatus,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              labelText: 'Invoice Status',
+                            ),
+                            items: invoiceStatus.map((String option) {
+                              return DropdownMenuItem(
+                                value: option,
+                                child: Text(option),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              if(newValue != null) {
+                                setState(() {
+                                  selectedItemCategory = newValue;
+                                });
+                              }
+                            },
+                          ),
+                          SizedBox(height: 20,),
+
+
+                          // Button
+                          SizedBox(
+                            width: double.infinity,
+                            height: 60,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey[400]
+                              ),
+                              onPressed: () {
+                              // handleInvoice();
+                              }, 
+                              child: AppText(title: "Submit", fontSize: 17, colors: Colors.black,)
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+        ),
+          // ),
         )
-      ),
     );
   }
 }
