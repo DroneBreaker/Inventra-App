258baec52d56ebbc21a9e8a37e978bf118bf54f3
DroneBreaker <brownierz01@gmail.com>
2025-07-14 09:36:55 +0100

    Minor change

--- mobile/lib/screens/expenses.dart
+++ mobile/lib/screens/expenses.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:inventra/config/app_colors.dart';
+import 'package:inventra/widgets/titles.dart';
 
 class ExpensesPage extends StatefulWidget {
   const ExpensesPage({super.key});
@@ -18,8 +22,35 @@ class _ExpensesPageState extends State<ExpensesPage> {
               top: 10,
               left: 8,
               child: IconButton(
-              onPressed: () => Navigator.pop(context), icon: Icon(Icons.arrow_back, size: 40,))
-            )
+              onPressed: () => Navigator.pop(context), icon: Icon(Icons.arrow_back, size: 40,)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20),
+              child: Column(
+                  children: [
+                    Gap(350.h),
+                    Center(
+                      child: appTitle(title: "THE EXPENSES PAGE IS UNDER CONSTRUCTION!"),
+                    ),
+                    Center(
+                      child: appParagraph(title: "We will be back soon!"),
+                    ),
+                    Gap(30.h),
+                    SizedBox(
+                      height: 60,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/home");
+                        }, 
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange
+                        ),
+                        child: appParagraph(title: "Go Back", color: AppColors.white)),
+                    )
+                  ],
+                ),
+            ),
           ],
         )
       ),
